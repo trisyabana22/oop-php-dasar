79c099c2543ccbb51ee6612bbcbe26591a175e56
Tri Syabana Octabiar <cobainaja1111@gmail.com>
2022-08-30 11:22:14 +0700

    menambahkan materi namespace

---
+++ namespace/App/Produk/Komik.php
@@ -0,0 +1,26 @@
+<?php
+
+class Komik extends Produk implements InfoProduk
+{
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = "harga", $jmlHalaman = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Komik :" . $this->getInfo() . "  - {$this->jmlHalaman} Halaman ";
+        return $str;
+    }
+
+    public function getInfo()
+    {
+        $str = "{$this->getLabel()} (Rp. {$this->harga}) ";
+
+
+        return $str;
+    }
+}
